38357a2acbadbfcea5daf5c692db46b50470b0d6
Carmen <maria.cisneros@laureate.mx>
2024-07-21 17:16:00 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+    var elementosNavBar = document.querySelectorAll('nav ul li a');
+
+// Itera sobre cada elemento y agrega un event listener
+    elementosNavBar.forEach(function(elemento) {
+        // Agrega un event listener para el evento 'click'
+        elemento.addEventListener('click', function() {
+            // Remueve la clase 'active' de todos los elementos
+            elementosNavBar.forEach(function(el) {
+                el.classList.remove('active');
+            });
+            // Agrega la clase 'active' solo al elemento clickeado
+            this.classList.add('active');
+        });
+    });
+
+    
+})(jQuery);
+
